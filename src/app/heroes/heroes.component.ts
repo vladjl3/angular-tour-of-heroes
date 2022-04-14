dcaa391a19c15a1e1968c8ac4423d661c5856c14
Vladislav Gusselnikov <vladjl3@yahoo.com>
2022-04-14 23:39:43 +0600

    Finished 2/6: Display a List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,9 +9,10 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
   }
 
   heroes = HEROES;
