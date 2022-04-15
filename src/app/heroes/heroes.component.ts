98aae49b97a74ac62234163398ef151030d1c249
Vladislav Gusselnikov <vladjl3@yahoo.com>
2022-04-15 11:14:33 +0600

    Implemented asynchronous data getting approach

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -19,7 +19,8 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   constructor(private heroService: HeroService) { }
