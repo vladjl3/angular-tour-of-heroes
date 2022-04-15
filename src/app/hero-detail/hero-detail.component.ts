e400a0fe817f18c4e952ba1ff1117cb77bc2b581
Vladislav Gusselnikov <vladjl3@yahoo.com>
2022-04-15 14:00:02 +0600

    Made HeroDetailComponent Routable

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+// Services
+import { HeroService } from '../hero.service';
+
+// Interfaces
 import { Hero } from '../hero';
 
 @Component({
@@ -10,9 +17,23 @@ export class HeroDetailComponent implements OnInit {
 
   @Input() hero?: Hero;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private heroService: HeroService
+  ) { }
 
   ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,17 +4,27 @@ import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
+import { Location } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
+  constructor(
+    private messageService: MessageService,
+    private location: Location
+    ) { }
+
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES)
-    this.messageService.add('HeroService: fetched heroes');
+    this.messageService.add(`HeroService: fetched heroes at ${this.location.path()}`);
     return heroes;
   }
 
-  constructor(private messageService: MessageService) { }
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero ${hero.name}, id=${id}, at ${this.location.path()}`)
+    return of(hero);
+  }
 }
